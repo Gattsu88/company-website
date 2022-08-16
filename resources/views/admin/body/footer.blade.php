f33a84d5a210edf3b33774c5af1d676ed1980c36
Gattsu88 <gattsufullon@gmail.com>
2022-08-17 00:01:31 +0200

    Admin panel setup

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © Made by Gattsu.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Berserk Design
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
